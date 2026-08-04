112e25492bc8efe3ed4f2c915bf2c37476a920a5
bobtrrr <bob_trrr@mail.ru>
2026-08-05 03:04:25 +0700

    feat: Добавит JS для site-nav

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const menuButton = document.querySelector('.main-nav__button');
+const menuContainer = document.querySelector('.main-nav');
+
+menuButton.addEventListener('click', () => {
+  menuContainer.classList.toggle('main-nav--opened');
+  menuButton.classList.toggle('toggle-button--opened');
+});
