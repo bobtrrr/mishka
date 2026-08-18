044018d077141bb659ddad8b71687d2c807f8d27
bob-ТРАКТОРИСТ! <108051824+bobtrrr@users.noreply.github.com>
2026-08-18 22:02:05 +0700

    feat: Добавит прогрессивное улучшения для меню - nojs (#11)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,10 @@
 const menuButton = document.querySelector('.main-nav__button');
 const menuContainer = document.querySelector('.main-nav');
 
+if (menuContainer.classList.contains('main-nav--nojs')) {
+  menuContainer.classList.remove('main-nav--nojs');
+}
+
 menuButton.addEventListener('click', () => {
   menuContainer.classList.toggle('main-nav--opened');
   menuButton.classList.toggle('toggle-button--opened');
