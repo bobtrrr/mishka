0ae685793bfe98b82c024c07b27d3237a9425b1f
bob-ТРАКТОРИСТ! <108051824+bobtrrr@users.noreply.github.com>
2026-08-25 05:32:04 +0700

    Feature/optimize (#19)
    * feat: Добавит базовую анимацию
    
    * feat: Добавит href для навигации
    
    * feat: Добавит анимацию modal
    
    * feat: Оптимизирует загрузку ресурсов
    
    * feat: Добавит доступность
    
    * feat: добавит favicon
    
    * feat: куча хлама

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,9 +3,14 @@ const menuContainer = document.querySelector('.main-nav');
 
 if (menuContainer.classList.contains('main-nav--nojs')) {
   menuContainer.classList.remove('main-nav--nojs');
+  menuButton.setAttribute('aria-expanded', false);
 }
 
 menuButton.addEventListener('click', () => {
   menuContainer.classList.toggle('main-nav--opened');
   menuButton.classList.toggle('toggle-button--opened');
+
+  const isExpand = menuButton.getAttribute('aria-expanded') === 'true';
+
+  menuButton.setAttribute('aria-expanded', !isExpand);
 });
